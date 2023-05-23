42c3947e7843463ea1655f0a9ff385e0b296aad8
UK <uk_kdw8794@naver.com>
2023-05-23 17:23:15 +0900

    commit

---
+++ src/Lv2/CodingTest06.java
@@ -0,0 +1,23 @@
+package Lv2;
+
+public class CodingTest06 {
+    /*
+     * n = 15
+     * 1 + 2 + 3 + 4 + 5 = 15
+     * 4 + 5 + 6 = 15
+     * 7 + 8 = 15
+     * 15 = 15
+     * result : 4
+     */
+    public int solution(int n) {
+        int answer = 0;
+
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        CodingTest06 ct06 = new CodingTest06();
+
+        System.out.println(ct06.solution(15));
+    }
+}
