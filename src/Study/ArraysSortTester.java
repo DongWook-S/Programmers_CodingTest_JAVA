ba2091ff861eff8525c16dab83577ab26c098f09
UK <uk_kdw8794@naver.com>
2023-06-21 16:32:41 +0900

    commit

---
+++ src/Study/ArraysSortTester.java
@@ -0,0 +1,31 @@
+package Study;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ *  Arrays.sort 메서드를 사용하여 정렬(퀵 정렬)
+ */
+public class ArraysSortTester {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+
+        int num = stdIn.nextInt();
+        int[] x = new int[num]; // 길이가 num 인 배열
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        Arrays.sort(x); // 배열 x를 정렬
+
+        System.out.println("오름차순으로 정렬 했습니다.");
+        for (int i = 0; i < num; i++) {
+            System.out.println("x[" + i + "] = " + x[i]);
+        }
+    }
+}
